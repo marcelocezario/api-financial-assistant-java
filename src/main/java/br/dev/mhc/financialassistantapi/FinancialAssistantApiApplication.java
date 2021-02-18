c99f4d7a0dc16e45fc5b83a4f01abdcbe350bc3f
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-18 19:51:20 -0300

    Project created

---
+++ src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.financialassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssistantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAssistantApiApplication.class, args);
+	}
+
+}
