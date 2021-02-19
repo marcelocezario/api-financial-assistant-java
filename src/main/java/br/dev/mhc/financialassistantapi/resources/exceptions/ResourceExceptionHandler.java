502ad2c7b06c75a1eb4625e4e531c04a44bbadcd
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-19 18:49:37 -0300

    Applying custom validations exceptions

--- src/main/java/br/dev/mhc/financialassistantapi/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/dev/mhc/financialassistantapi/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.dev.mhc.financialassistantapi.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +21,25 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
-		
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<StandardError> validation(ConstraintViolationException e, HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error",
+				System.currentTimeMillis());
+
+		for (ConstraintViolation<?> x : e.getConstraintViolations()) {
+			err.addError(x.getPropertyPath().toString(), x.getMessage());
+		}
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
