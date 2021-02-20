0d5282389a47dea514f427702c27ebccfba82383
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-20 11:18:28 -0300

    CommandLineRunner for tests

--- src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
+++ src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
@@ -1,13 +1,17 @@
 package br.dev.mhc.financialassistantapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinancialAssistantApiApplication {
+public class FinancialAssistantApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialAssistantApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
