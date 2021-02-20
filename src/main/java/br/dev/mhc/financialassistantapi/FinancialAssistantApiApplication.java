c1525eb463d824930b7cab4425be38fc2fa2eccf
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-20 11:18:34 -0300

    Revert "CommandLineRunner for tests"
    This reverts commit 0d5282389a47dea514f427702c27ebccfba82383.

--- src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
+++ src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
@@ -1,17 +1,13 @@
 package br.dev.mhc.financialassistantapi;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinancialAssistantApiApplication implements CommandLineRunner {
+public class FinancialAssistantApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialAssistantApiApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
 }
