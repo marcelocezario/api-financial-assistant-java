0f654e76913828309bfd653e6d948bfa34039ff2
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-22 10:37:48 -0300

    Applying CommandLineRunner for tests

--- src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
+++ src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
@@ -1,13 +1,33 @@
 package br.dev.mhc.financialassistantapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FinancialAssistantApiApplication {
+import br.dev.mhc.financialassistantapi.entities.User;
+import br.dev.mhc.financialassistantapi.services.UserService;
 
+@SpringBootApplication
+public class FinancialAssistantApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialAssistantApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User marcelo = userService.findByEmail("marcelo@gmail.com");
+		System.out.println(marcelo);
+		
+		User teste = userService.findByEmail("testeteste@gmail.com");
+		System.out.println(teste);
+		
+		
 	}
 
 }
