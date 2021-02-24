424b16bcfa44a9b0290745dc7c866ae30e54055a
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-24 10:01:52 -0300

    Initial Spring Security settings

--- src/main/java/br/dev/mhc/financialassistantapi/config/SecurityConfig.java
+++ src/main/java/br/dev/mhc/financialassistantapi/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+			"/outrosendpointsliberados/**"
+	};
+
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/users/**",
+			"/outrosendpointscomapenasgetliberado/**"
+	};
+
 	@Autowired
 	private Environment env;
 
@@ -29,7 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
 	}
 
 	@Bean
