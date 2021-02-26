5b2448e60b9dbf6214133e98dc73e8f07984052a
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-26 13:37:19 -0300

    Handling Amazon exceptions

--- src/main/java/br/dev/mhc/financialassistantapi/services/S3Service.java
+++ src/main/java/br/dev/mhc/financialassistantapi/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.dev.mhc.financialassistantapi.services.exceptions.FileException;
+
 @Service
 public class S3Service {
 
@@ -34,7 +36,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("IO error: " + e.getMessage());
+			throw new FileException("IO error: " + e.getMessage());
 		}
 	}
 
@@ -47,7 +49,7 @@ public class S3Service {
 			LOG.info("Upload completed");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Error converting URL to URI");
+			throw new FileException("Error converting URL to URI");
 		}
 	}
 
