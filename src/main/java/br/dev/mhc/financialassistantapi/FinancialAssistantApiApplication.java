01c3c331722e34fa28e0bd11a2582811f9f4ff3d
Marcelo Cezário <marcelocezario@gmail.com>
2021-02-26 09:23:03 -0300

    Sending file to AmazonS3

--- src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
+++ src/main/java/br/dev/mhc/financialassistantapi/FinancialAssistantApiApplication.java
@@ -1,11 +1,17 @@
 package br.dev.mhc.financialassistantapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.dev.mhc.financialassistantapi.services.S3Service;
+
 @SpringBootApplication
 public class FinancialAssistantApiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialAssistantApiApplication.class, args);
@@ -13,6 +19,6 @@ public class FinancialAssistantApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\teste\\teste.jpg");
 	}
-
 }
