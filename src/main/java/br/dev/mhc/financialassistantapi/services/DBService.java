c9c2287be77609747f31eb69dd6392e1b1d5dc6a
Marcelo Cezário <marcelocezario@gmail.com>
2021-03-12 18:58:36 -0300

    Initial loading database for testing

--- src/main/java/br/dev/mhc/financialassistantapi/services/DBService.java
+++ src/main/java/br/dev/mhc/financialassistantapi/services/DBService.java
@@ -8,8 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.dev.mhc.financialassistantapi.entities.Account;
+import br.dev.mhc.financialassistantapi.entities.Category;
+import br.dev.mhc.financialassistantapi.entities.Entry;
 import br.dev.mhc.financialassistantapi.entities.User;
+import br.dev.mhc.financialassistantapi.entities.accounts.BankAccount;
+import br.dev.mhc.financialassistantapi.entities.accounts.CreditCard;
+import br.dev.mhc.financialassistantapi.entities.accounts.Wallet;
+import br.dev.mhc.financialassistantapi.entities.enums.EntryType;
 import br.dev.mhc.financialassistantapi.entities.enums.Profile;
+import br.dev.mhc.financialassistantapi.repositories.AccountRepository;
+import br.dev.mhc.financialassistantapi.repositories.CategoryRepository;
+import br.dev.mhc.financialassistantapi.repositories.EntryRepository;
 import br.dev.mhc.financialassistantapi.repositories.UserRepository;
 
 @Service
@@ -20,6 +30,15 @@ public class DBService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private EntryRepository entryRepository;
 
 	public void instantiateTestDatabase() throws ParseException {
 
@@ -38,7 +57,35 @@ public class DBService {
 		User u6 = new User(null, "Carlos José Cezario", "Carlos", "carlos@gmail.com", pe.encode("666666"),
 				Instant.parse("2021-02-06T10:00:00Z"), null);
 
+		Account a1 = new Wallet(null, "Carteira", 0.0, u1);
+		Account a2 = new BankAccount(null, "Conta corrente", 0.0, 8.0, 1000.0, u1);
+		Account a3 = new CreditCard(null, "Cartão de crédito", 0.0, 15, 25, 200.0, u1);
+		Account a4 = new Wallet(null, "Carteira", 0.0, u2);
+		Account a5 = new BankAccount(null, "Conta corrente", 0.0, 10.0, 500.0, u2);
+		Account a6 = new CreditCard(null, "Cartão de crédito", 0.0, 10, 20, 500.0, u2);
+
+		Category c1 = new Category(null, "Alimentação", "icon-alim.svg", u1);
+		Category c2 = new Category(null, "Combustível", "icon-vehicle.svg", u1);
+		Category c3 = new Category(null, "Trabalho", "icon-work.svg", u1);
+
+		Category c4 = new Category(null, "Alimentação", "icon-alim.svg", u2);
+		Category c5 = new Category(null, "Combustível", "icon-vehicle.svg", u2);
+		Category c6 = new Category(null, "Trabalho", "icon-work.svg", u2);
+		
+		Entry e1 = new Entry(null, Instant.now(), 10.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		Entry e2 = new Entry(null, Instant.now(), 15.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		Entry e3 = new Entry(null, Instant.now(), 20.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		Entry e4 = new Entry(null, Instant.now(), 25.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		Entry e5 = new Entry(null, Instant.now(), 30.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		Entry e6 = new Entry(null, Instant.now(), 50.0, "Dinheiro recebido", Instant.now(), Instant.now(), 1, 1, EntryType.CREDIT, a2, u2, null);
+		Entry e7 = new Entry(null, Instant.now(), 10.0, "Compra lanche", Instant.now(), Instant.now(), 1, 1, EntryType.DEBIT, a2, u2, null);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+		accountRepository.saveAll(Arrays.asList(a1, a2, a3, a4, a5, a6));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
+		entryRepository.saveAll(Arrays.asList(e1, e2, e3, e4, e5, e6, e7));
+		
+
 	}
 
 }
