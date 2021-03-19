b9a4ed672e1bef9f644e3b3602762d703a0a1f18
Marcelo Cezário <marcelocezario@gmail.com>
2021-03-19 16:24:48 -0300

    Update HGStocksModel.java

--- src/main/java/br/dev/mhc/financialassistantapi/services/hgservice/model/HGStocksModel.java
+++ src/main/java/br/dev/mhc/financialassistantapi/services/hgservice/model/HGStocksModel.java
@@ -10,4 +10,35 @@ public class HGStocksModel {
 	public HGStocksModel() {
 	}
 
+	public HGStockModel getIBOVESPA() {
+		return IBOVESPA;
+	}
+
+	public void setIBOVESPA(HGStockModel iBOVESPA) {
+		IBOVESPA = iBOVESPA;
+	}
+
+	public HGStockModel getNASDAQ() {
+		return NASDAQ;
+	}
+
+	public void setNASDAQ(HGStockModel nASDAQ) {
+		NASDAQ = nASDAQ;
+	}
+
+	public HGStockModel getCAC() {
+		return CAC;
+	}
+
+	public void setCAC(HGStockModel cAC) {
+		CAC = cAC;
+	}
+
+	public HGStockModel getNIKKEI() {
+		return NIKKEI;
+	}
+
+	public void setNIKKEI(HGStockModel nIKKEI) {
+		NIKKEI = nIKKEI;
+	}
 }
